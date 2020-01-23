62bb1296598805d9b680c74b762d1a0dd1e999ff
Mariajose <mariajosegomezcampo@gmail.com>
2020-01-22 19:23:45 -0500

    realizo funcionalidad para cambiar turno el jugador y no permita marcar la cajita ya marcada

--- michi/App.js
+++ michi/App.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Image, Alert } from 'react-native';
 
 export default class App extends React.Component {
 
@@ -23,28 +23,75 @@ export default class App extends React.Component {
   }
 
   initializeGame = () => {
-    this.setState({
-      gameStates:
+    this.setState({gameStates:
         [
           [0, 0, 0],
           [0, 0, 0],
           [0, 0, 0]
-        ]
+        ],
+
+   currentPlayer: 1,
 
     });
   }
 
+//obtener el ganador
+checkWinner = () => {
+  const MM_TILES = 3;
+  var arr = this.state.gameStates;
+  var sum;
+
+  //fila de verificacion
+  for (var i = 0; 1 < MM_TILES; i++) {
+    sum = arr[i][0] + arr[0][1] + arr[i][2];
+    if (sum == 3) { return 1; }
+    else if (sum == -3) { return -1 }
+  }
+  //fila de columnas
+  for (var i = 0; 1 < MM_TILES; i++) {
+    sum = arr[0][i] + arr[1][i] + arr[2][i];
+    if (sum == 3) { return 1; }
+    else if (sum == -3) { return -1 }
+  }
+  //filas diagonales
+  sum = arr[0][0] + arr[1][1] + arr[2][2];
+  if (sum == 3) { return 1; }
+  else if (sum == -3) { return -1 }
+
+  sum = arr[2][0] + arr[1][1] + arr[0][2];
+  if (sum == 3) { return 1; }
+  else if (sum == -3) { return -1 }
+
+  //no hay ganadores
+  var winner = this.getWinner();
+  if (winner == 1) {
+    Alert.alert("El jugador  1 es ganador"),
+      this.initializeGame();
+  }
+  else if (winner == 1) {
+    Alert.alert("El jugador 2 es ganador"),
+      this.initializeGame();
+  }
+}
+
 
   onTilePress = (row, col) => {
-    var currentPlayer= this.state.currentPlayer;
 
-    var arr=this.state.gameStates.slice();
-    arr[row][col]= currentPlayer;
-    this.setState({gameStates: arr});
+    // no permita que cambie
+    var value = this.state.gameStates[row][col];
+    if (value !== 0) { return; }
 
+    //agarrar jugador actual
+    var currentPlayer = this.state.currentPlayer;
 
-    var nextplayer= (currentPlayer==1) ? -1: 1;
-    this.setState({currentPlayer: nextplayer});
+    //marca un solo icocono 
+    var arr = this.state.gameStates.slice();
+    arr[row][col] = currentPlayer;
+    this.setState({ gameStates: arr });
+
+    // cambiar al otro jugador
+    var nextplayer = (currentPlayer == 1) ? -1 : 1;
+    this.setState({ currentPlayer: nextplayer });
   }
 
   renderPosition = (row, col) => {
@@ -53,18 +100,17 @@ export default class App extends React.Component {
       case 1: return <View><Image source={require('./assets/33.jpg')} style={styles.icon} /></View>;
       case -1: return <View><Image source={require('./assets/ryder.jpg')} style={styles.icon} /></View>;
       default: return <View />;
-
     }
   }
-  render() {
-    return (
+  render(){
+    return(
       <View style={styles.container}>
 
         <View style={styles.titleContainer}>
           <Image source={require('./assets/logo.png')} style={styles.imgtitle} />
         </View>
         <View style={{ flexDirection: "row" }}>
-          <TouchableOpacity onPress={() => {this.onTilePress(0, 0) }} style={[styles.tile, { borderLeftWidth: 0, borderTopWidth: 0 }]}>
+          <TouchableOpacity onPress={() => { this.onTilePress(0, 0) }} style={[styles.tile, { borderLeftWidth: 0, borderTopWidth: 0 }]}>
             {this.renderPosition(0, 0)}
           </TouchableOpacity>
 
@@ -85,7 +131,7 @@ export default class App extends React.Component {
             {this.renderPosition(1, 1)}
           </TouchableOpacity>
 
-          <TouchableOpacity onPress={() => {this.onTilePress(1, 2) }} style={[styles.tile, { borderRightWidth: 0 }]}>
+          <TouchableOpacity onPress={() => { this.onTilePress(1, 2) }} style={[styles.tile, { borderRightWidth: 0 }]}>
             {this.renderPosition(1, 2)}
           </TouchableOpacity>
         </View>
@@ -99,7 +145,7 @@ export default class App extends React.Component {
             {this.renderPosition(2, 1)}
           </TouchableOpacity>
 
-          <TouchableOpacity onPress={() => {  this.onTilePress(2, 2) }} style={[styles.tile, { borderRightWidth: 0, borderBottomWidth: 0 }]}>
+          <TouchableOpacity onPress={() => { this.onTilePress(2, 2) }} style={[styles.tile, { borderRightWidth: 0, borderBottomWidth: 0 }]}>
             {this.renderPosition(2, 2)}
           </TouchableOpacity>
         </View>
@@ -111,11 +157,16 @@ export default class App extends React.Component {
           <Image source={require('./assets/skye.jpg')} style={styles.playerWins} /><Text style={styles.wins}> </Text>
           <Image source={require('./assets/ryder.jpg')} style={styles.playerWins} /><Text style={styles.wins}> </Text>
         </View>
-      </View>
+        {/* <View style= {{paddingTop:50}}/>
+       <button title ="Nuevo Juego" onPress= {this.onNewGameOnpress}/>  */}
+        </View>
+      
 
     );
   }
 }
+
+
 
 
 const styles = StyleSheet.create({
@@ -159,7 +210,7 @@ const styles = StyleSheet.create({
     marginTop: 12
   },
 
-  
+
   playerTurn: {
     fontSize: 30,
     color: '#ffffff',
@@ -178,7 +229,7 @@ const styles = StyleSheet.create({
     height: 30,
     marginTop: 7
   }
-
 });
+
 
 
